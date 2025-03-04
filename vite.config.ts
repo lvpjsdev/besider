355c38a9a9788986ab802c25a948471cd32cecd1
Leonid Petrov <lvpjsdev@gmail.com>
2025-03-04 21:02:09 +0300

    fix: change commit

--- vite.config.ts
+++ vite.config.ts
@@ -20,23 +20,7 @@ export default defineConfig({
       '/api': {
         target: 'https://api.nytimes.com/svc/archive/v1/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-
-        configure: (proxy, options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('error', err);
-          });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Request sent to target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log(
-              'Response received from target:',
-              proxyRes.statusCode,
-              req.url
-            );
-          });
-        },
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
